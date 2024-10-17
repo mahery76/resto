9edab00897b879ffb4a5017dfc7e3013eafdfe0b
Mahery Antonio <109660691+mahery76@users.noreply.github.com>
2024-10-17 11:24:45 +0300

    feat: migrate database

--- resto.infrastructure/Migrations/20241017081804_initial5.Designer.cs
+++ resto.infrastructure/Migrations/20241017081804_initial5.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using resto.infrastructure.Data;
 
@@ -10,9 +11,11 @@ using resto.infrastructure.Data;
 namespace resto.infrastructure.Migrations
 {
     [DbContext(typeof(ProduitContext))]
-    partial class ProduitContextModelSnapshot : ModelSnapshot
+    [Migration("20241017081804_initial5")]
+    partial class initial5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
---
+++ resto.infrastructure/Migrations/20241017081804_initial5.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace resto.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Prix_produit",
+                table: "Produits",
+                newName: "Prix");
+
+            migrationBuilder.RenameColumn(
+                name: "Nom_Produit",
+                table: "Produits",
+                newName: "Nom");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Prix",
+                table: "Produits",
+                newName: "Prix_produit");
+
+            migrationBuilder.RenameColumn(
+                name: "Nom",
+                table: "Produits",
+                newName: "Nom_Produit");
+        }
+    }
+}
